f86881ec3060ab4f0c51eba4802027e1786312c6
guilherme <progr.guilherme@gmail.com>
2023-12-16 04:40:59 +0000

    basic project setup with initial  roadmap

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Roadmap
+
+This is early development version. I am currently considering:
+- [ ] Parse two arguments `action` and `action_name`.
+- [ ] implement `screen homeScreen` command.
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,30 @@ struct Cli {
 
 // Eg: cargo run -- main src/main.rs
 fn main() {
+    // call eg function
+    // eg_function();
+
+    // - cargo run -- route src/generated/generic_file.txt
+    // find_pattern_in_file();
+    
+}
+
+#[allow(dead_code)]
+fn find_pattern_in_file() {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+}
+
+#[allow(dead_code)]
+fn eg_function() {
+    /*mut anotation*/
+    let mut a = 'a';
+    println!("{}", a);
+    a = 'b';
+    println!("{}", a);
 }
