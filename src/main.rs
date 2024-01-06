8f58d892c5c634612c9607a19bd8656ec2a9b6d1
guilherme henrique machado <progr.guilherme@gmail.com>
2024-01-06 18:21:28 -0300

    chore: remove unused and comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::fs;
 use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -11,19 +11,13 @@ struct Cli {
     extension: String,
 }
 
-// how to run:
-// cargo run -- screen homeScreen tsx
-// cargo run -- atom homeAtom ts
-// cargo run -- component homeComponent tsx
-
 fn create_file_buf(folder: &String, filename: &String, ext: &String) -> PathBuf {
     let mut path = PathBuf::new();
     path.push(folder);
     path.push(filename);
     path.set_extension(ext);
 
-
-    return path
+    return path;
 }
 
 fn check_if_folder_exists(folder: &String) -> bool {
@@ -31,7 +25,6 @@ fn check_if_folder_exists(folder: &String) -> bool {
     let exist_path = fs::metadata(&folder_path).is_ok();
     println!("exist path: {}", exist_path);
     return exist_path;
-    // return fs::metadata(format!("./{}", folder)).unwrap().is_file();
 }
 
 fn create_folder(folder: &String) {
@@ -45,23 +38,21 @@ fn main() -> std::io::Result<()> {
     
     let file_buf = create_file_buf(&action, &file_name, &extension);
     let folder_exists = check_if_folder_exists(&action);
-    // let file_exists = check_if_file_exists(&formated_file_name);
 
-
-    // create folder if it doesn't exist
+    // Create folder if it doesn't exist
     if folder_exists {
-        println!("folder exists");
+        println!("Folder exists");
     } else {
         create_folder(&action);
-        println!("folder created");
+        println!("Folder created");
     }
 
+    // Create file if it doesn't exist
     if file_buf.exists() {
         println!("File exists!")
     } else {
-        let mut _file = File::create(&file_buf).expect("Unable to create file");
-        _file.write_all("Hello, world!".as_bytes()).expect("Unable to write data");
-    // file.write_all("Hello, world!".as_bytes())?;
+        let mut file = File::create(&file_buf).expect("Unable to create file");
+        file.write_all("Hello, world!".as_bytes()).expect("Unable to write data");
     }
 
     Ok(())
